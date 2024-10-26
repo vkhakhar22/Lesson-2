85ab7601828d6588e29f7cfc10db4c3f3fa7e69f
vkhakhar22 <vanshikhakhar22.svkm@gmail.com>
2024-10-26 13:18:42 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,15 @@
+public class Operations {
+    public static void main(String[] args){
+        int a=10;
+        int b=4;
+        int sum=a+b;
+        int diff=a-b;
+        int mult=a*b;
+        int div=a/b;
+        System.out.println("Addition of a and b is" +sum);
+        System.out.println("Subtraction of a and b is " + diff);
+        System.out.println("Multiplication of a and b is " +mult);
+        System.out.println("Division of a and b is " +div);
+        
+    }
+}
